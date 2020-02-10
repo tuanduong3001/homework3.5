5bd629667f0e8c3266d5ce998dc76558cfb94de0
Tuan Duong <56602084+tuanduong3001@users.noreply.github.com>
2020-02-10 22:27:13 +0700

    Add files via upload

---
+++ 2.23.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<math.h>
+
+
+using namespace std;
+int isPrime(int n)
+{
+	for (int i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+			return 0;
+	}
+	return 1;
+}
+int countNumber(int n)
+{
+	int count = 0;
+	for (int i = 2; i <= n; i++)
+	{
+		if (n % i == 0 && isPrime(i))
+			count++;
+	}
+	return count;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	cout << countNumber(n);
+
+}
